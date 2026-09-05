9bfab77211566c6540b476eed60241dfc190a049
Sm4rtBoyTom <155878804+Sm4rtBoyTom@users.noreply.github.com>
2026-09-05 12:40:50 +0200

    Add files via upload

---
+++ VisualStudio/LWUtilities.cs
@@ -0,0 +1,51 @@
+﻿
+namespace Leatherworks
+{
+    internal static class LWUtilities
+    {
+        public static Material TanningLiquidMaterial()
+        {
+            Texture2D LiquidTexture = LWMain.LWTexturesBundle.LoadAsset<Texture2D>("T_Tanning_Cooking");
+            Material LiquidMaterial = new Material(GearItem.LoadGearItemPrefab("GEAR_CoffeeCup").gameObject.GetComponent<Cookable>().m_CookingPotMaterialsList[0]);
+            LiquidMaterial.SetTexture("_Main_texture2", LiquidTexture);
+            return LiquidMaterial;
+        }
+        public static Material BarkNoodlesLiquidMaterial()
+        {
+            Texture2D LiquidTexture = LWMain.LWTexturesBundle.LoadAsset<Texture2D>("T_BarkNoodles_Cooking");
+            Material LiquidMaterial = new Material(GearItem.LoadGearItemPrefab("GEAR_CoffeeCup").gameObject.GetComponent<Cookable>().m_CookingPotMaterialsList[0]);
+            LiquidMaterial.SetTexture("_Main_texture2", LiquidTexture);
+            return LiquidMaterial;
+        }
+        public static Material BirchBarkNoodlesLiquidMaterial()
+        {
+            Texture2D LiquidTexture = LWMain.LWTexturesBundle.LoadAsset<Texture2D>("T_BirchBarkNoodles_Cooking");
+            Material LiquidMaterial = new Material(GearItem.LoadGearItemPrefab("GEAR_CoffeeCup").gameObject.GetComponent<Cookable>().m_CookingPotMaterialsList[0]);
+            LiquidMaterial.SetTexture("_Main_texture2", LiquidTexture);
+            return LiquidMaterial;
+        }
+        public static Material BarkBannockRawGrubMaterial()
+        {
+            Texture2D LiquidTexture = LWMain.LWTexturesBundle.LoadAsset<Texture2D>("T_BarkBannock_Raw");
+            Material LiquidMaterial = new Material(GearItem.LoadGearItemPrefab("GEAR_UncookedBannock").gameObject.GetComponent<Cookable>().m_CookingPotMaterialsList[0]);
+            LiquidMaterial.SetTexture("_Main_texture2", LiquidTexture);
+            return LiquidMaterial;
+        }
+
+        public static AssetBundle LoadFromStream(string name) // Asset Bundle Loader
+        {
+            using (Stream? stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(name))
+            {
+                MemoryStream? memory = new((int)stream.Length);
+                stream!.CopyTo(memory);
+
+                Il2CppSystem.IO.MemoryStream memoryStream = new Il2CppSystem.IO.MemoryStream(memory.ToArray());
+
+                AssetBundle loadFromMemoryInternal = AssetBundle.LoadFromStream(memoryStream);
+                return loadFromMemoryInternal;
+            }
+        }
+
+    }
+}
+
---
+++ VisualStudio/Settings.cs
@@ -0,0 +1,57 @@
+﻿namespace Leatherworks
+{
+    internal class Settings : JsonModSettings
+    {
+        internal static Settings instance = new Settings();
+
+        [Section("Spawn Chances")]
+
+        [Name("Treebark spawn chance")]
+        [Description("Choose How Often Treebark Should Spawn. Default: 10%")]
+        [Slider(1, 25, 25)]
+        public int treebarkChance = 10;
+
+        [Section("Leather Yield")]
+
+        [Name("Rabbit leather yield")]
+        [Description("Choose How Much Scraped Leather You Get From A Rabbit Pelt. Default: 1")]
+        [Slider(1, 5, 5)]
+        public int rabbitYield = 1;
+
+        [Name("Deer and wolf leather yield")]
+        [Description("Choose How Much Scraped Leather You Get From A Deer Hide and Wolf Pelt. Default: 3")]
+        [Slider(1, 5, 5)]
+        public int wolfdeerYield = 3;
+
+        [Name("Bear and moose leather yield")]
+        [Description("Choose How Much Scraped Leather You Get From A Moose and Bear Hide. Default: 5")]
+        [Slider(1, 5, 5)]
+        public int moosebearYield = 5;
+
+        [Name("Cougar leather yield")]
+        [Description("Choose How Much Scraped Leather You Get From A Cougar Hide. Default: 4")]
+        [Slider(1, 5, 5)]
+        public int cougarYield = 4;
+
+        [Section("Preferences")]
+
+        [Name("Bark amount for making flour")]
+        [Description("Choose How Much Fried Tree Bark Is Needed For Making Flour. Default: 35")]
+        [Slider(25, 60, 8)]
+        public int flourAmount = 35;
+
+        [Name("Tanning amount to fill a box")]
+        [Description("Choose How Much Tanning Is Needed For Filling A Metal Box. Default: 1")]
+        [Slider(1, 4, 4)]
+        public int tanningAmount = 1;
+
+        [Name("Able to Scrape Fresh Hide?")]
+        [Description("Enable The Ability To Scrape Fresh Hide. Default: False")]
+        public bool noCured = false;
+
+        [Name("Able to Use Stone To Grind Flour?")]
+        [Description("Enable The Ability To Grind Flour Using A Stone. Default: False")]
+        public bool noGrind = false;
+
+    }
+}
