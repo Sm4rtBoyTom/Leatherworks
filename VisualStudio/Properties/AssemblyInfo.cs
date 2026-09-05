ed05cfa150ac3e7db808a38b4d94044aed96dc48
Sm4rtBoyTom <155878804+Sm4rtBoyTom@users.noreply.github.com>
2026-09-05 12:40:19 +0200

    Delete VisualStudio directory

--- VisualStudio/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-[assembly: AssemblyTitle("Leatherworks")]
-[assembly: AssemblyCopyright("Sm4rtBoyTom")]
-[assembly: AssemblyVersion("1.3.1")]
-[assembly: AssemblyFileVersion("1.3.1")]
-[assembly: MelonInfo(typeof(Leatherworks.LWMain), "Leatherworks", "1.3.1", "Sm4rtBoyTom, TonisGaming", null)]
-[assembly: MelonGame("Hinterland", "TheLongDark")]
